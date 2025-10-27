cd9f1fda9503a50e5155f67b1ee8c19c63817cdb
Eoghan Breen <Breen-E18@ulster.ac.uk>
2025-10-27 10:45:26 +0000

    first attempt push to see if works

---
+++ Assets/_Game/Code/SpikeScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SpikeScript : MonoBehaviour
+{
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+          //  Destroy(Player);
+        }
+    }
+}
